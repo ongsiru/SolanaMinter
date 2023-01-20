50009a07f2fb8628c4adbbead2f2daa3501a9b92
ongsiru <gusdnr3998@gmail.com>
2023-01-20 10:44:03 +0900

    add wagami

--- src/Faq.tsx
+++ src/Faq.tsx
@@ -1,23 +1,42 @@
-import React from 'react';
-import Accordian from './accordian';
+// import React from 'react';
+// import Accordian from './accordian';
 
-function Faq() {
-    return (
-        <div id='faq' style={{ backgroundColor: 'black', width: '100vw', height: '100vh', position: 'relative', top: '0', overflow: 'hidden' }}>
-            <div style={{ width: '100%', position: 'absolute', top: '15%', margin: '0 auto', display: 'flex', flexDirection: 'column' }}>
-            <Accordian />
-          <div style={{paddingTop:'30px'}}></div>
-          <Accordian />
-          <div style={{paddingTop:'30px'}}></div>
-          <Accordian />
-          <div style={{paddingTop:'30px'}}></div>
-          <Accordian />
-          <div style={{paddingTop:'30px'}}></div>
-          <Accordian />
-            </div>
+import { WagmiConfig, createClient } from 'wagmi'
+import { getDefaultProvider } from 'ethers'
+import Profile from './Profile'
 
-        </div>
-    )
-};
+const client = createClient({
+  autoConnect: true,
+  provider: getDefaultProvider(),
+})
 
-export default Faq;
+function App() {
+  return (
+    <WagmiConfig client={client}>
+      <Profile />
+    </WagmiConfig>
+  )
+}
+
+export default App;
+
+// function Faq() {
+//     return (
+//         <div id='faq' style={{ backgroundColor: 'black', width: '100vw', height: '100vh', position: 'relative', top: '0', overflow: 'hidden' }}>
+//             <div style={{ width: '100%', position: 'absolute', top: '15%', margin: '0 auto', display: 'flex', flexDirection: 'column' }}>
+//             <Accordian />
+//           <div style={{paddingTop:'30px'}}></div>
+//           <Accordian />
+//           <div style={{paddingTop:'30px'}}></div>
+//           <Accordian />
+//           <div style={{paddingTop:'30px'}}></div>
+//           <Accordian />
+//           <div style={{paddingTop:'30px'}}></div>
+//           <Accordian />
+//             </div>
+
+//         </div>
+//     )
+// };
+
+// export default Faq;
